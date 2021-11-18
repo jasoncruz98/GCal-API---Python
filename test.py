68d1e27094e60b4420861dfa7ae15e66e44d6432
Jason Cruz <jasoncruz98@gmail.com>
2021-11-19 06:57:32 +0800

    created file for implementing GCal aPI using OOP, and removed token.json from remote repo

---
+++ test.py
@@ -0,0 +1,57 @@
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+client_secret_file = 'client_secret_file.json'
+
+
+class GCal:
+    creds = None
+    API_NAME = 'calendar'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    def __init__(self, client_secret_file):
+        self.client_secret_file = client_secret_file
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file(
+                'token.json', self.SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.client_secret_file, self.SCOPES)
+            self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(self.creds.to_json())
+
+        self.service = build(self.API_NAME, self.API_VERSION,
+                             credentials=self.creds)
+
+    def getEventsList(self):
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = self.service.events().list(calendarId='primary', timeMin=now,
+                                                   maxResults=10, singleEvents=True,
+                                                   orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+
+g = GCal("client_secret_file.json")
+g.getEventsList()
